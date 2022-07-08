fa7bb0bcfc8d351cfa103cb35ca13f066b7ef1ae
Selmon123 <sudhil.sujith7384@gmail.com>
2022-07-08 12:29:55 +0530

    myntra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewshoppingComponent } from './viewshopping/viewshopping.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewshoppingComponent
   ],
   imports: [
     BrowserModule,
